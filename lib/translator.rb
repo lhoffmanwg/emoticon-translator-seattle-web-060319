be72406eec3cf360b1b6c7319d80381b016e2094
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-21 03:52:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,10 +21,11 @@ end
 def get_japanese_emoticon(path, emoticon)
  emoticon_hash = load_library(path)
  emoticon_hash.each do |key, value|
-   if key == "get_meaning"
-    value.each do |j_emoticon, meaning|
-    binding.pry
-   end   
+  # if key == "get_meaning"
+  #  value.each do |j_emoticon, meaning|
+    if key == "get_emoticon"
+      binding.pry
+    end   
   end
  end
   #emoticons.each do |meaning, ary|
