dfd4d3bf3345dfc234f6ba3ed8f8822bca47f8d2
Laura Hoffman <lhoffmanwg@gmail.com>
2019-04-21 04:14:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(path, emoticon)
  emoticon_hash = load_library(path)
    result = emoticon_hash["get_emoticon"][emoticon]
-   binding.pry
+  
   if result == nil
     result = "Sorry, that emoticon was not found" 
   end
